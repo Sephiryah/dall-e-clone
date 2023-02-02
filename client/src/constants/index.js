8d9922527d0cfa25b815ee70f32e955338689707
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-02 09:46:09 +0100

    Add constant prompts and remove react.svg

---
+++ client/src/constants/index.js
@@ -0,0 +1,12 @@
+export const surpriseMePrompts = [
+    "potato trying to learn how to code",
+    "a horse tame a human",
+    "a dog playing badly Poker",
+    "a human sleeping in the dog's bed",
+    "cats invade the Earth",
+    "a programmer fighting bug in his code",
+    "a storyteller imagining an epic story with dragons and giant monsters",
+    "a dj doing a concert on Mars",
+    "Roman fighting vikings",
+    "A guy overpowered killing everyone with his fingers only"
+];
