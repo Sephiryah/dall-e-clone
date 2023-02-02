fcf899f9caee3552bb79bbb9701078e1859a3d5a
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-02 10:13:08 +0100

    Import react-router-dom component & logo in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+
 const App = () => {
   return (
     <h1 className="font-bold">Hello World! I'm Dall-E!</h1>
