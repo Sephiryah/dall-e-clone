686e3eaa2806d09306f7dfec9da898bc11864e21
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-02 14:53:21 +0100

    Add Loader.jsx in components and import all of them in Home.jsx

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from './Card.jsx';
+import FormField from './Formfield.jsx';
+import Loader from './Loader.jsx';
+
+export {
+    Card,
+    FormField,
+    Loader
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react';
 
+import { Card, FormField, Loader } from '../components';
+
 function Home() {
     return (
         <div>Home</div>
