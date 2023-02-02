f35ff360aeaa1681299afba7be7c85b744f2abae
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-02 15:56:52 +0100

    Add text for search result in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,10 @@ import {useState, useEffect} from 'react';
 import { Card, FormField, Loader } from '../components';
 
 function Home() {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [ allPosts, setAllPosts] = useState(null);
+
+    const [searchText, setSearchText] = useState('test');
 
     return (
         <section>
@@ -17,13 +19,16 @@ function Home() {
                 <FormField/>
             </div>
 
-            <div className="w-[50px]">
+            <div>
                 {loading ? (
-                    <div>
+                    <div  className="w-[50px]">
                         <Loader />
                     </div>
                 ) : (
                     <>
+                    {searchText && (
+                        <h2 className="text-[18px] font-bold">Showing Result for <span>{searchText}</span></h2>
+                    )}
                     </>
                 )}
             </div>
