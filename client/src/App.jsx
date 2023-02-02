30936f68e284fbb62ccc9cb69a88ad0870c437b9
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-02 11:59:01 +0100

    Add style for header and logo in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <header>
+      <header className="bg-[#3a405a] flex justify-between items-center pr-5 pl-3 pt-3 pb-3 shadow-black shadow-[0_3px_5px_black] border-b-[1px] border-[#111]">
         <Link to="/">
-          <img src={logo} alt="Dall-F logo"/>
+          <img src={logo} alt="Dall-F logo" className="h-[50px]"/>
         </Link>
         <Link to="/create-post">Create</Link>
       </header>
