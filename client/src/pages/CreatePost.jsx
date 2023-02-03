289371f410fa8e37413ea2491ca095c755df52c6
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-03 09:41:35 +0100

    Add & import getRandomPrompt in CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 function CreatePost() {
