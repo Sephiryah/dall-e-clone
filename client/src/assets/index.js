f519721b804c1fac60981e02815891c93d90fcdf
Sephirah <nicolas.bussiere@outlook.fr>
2023-02-03 09:35:16 +0100

    Import Hook, preview.svg and components in CreatePost.jsx

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
+import preview from "./preview.svg";
 
 export  {
-    logo
+    logo,
+    preview
 };
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { FormField, Loader } from '../components';
+
 function CreatePost() {
     return (
         <div>CreatePost</div>
